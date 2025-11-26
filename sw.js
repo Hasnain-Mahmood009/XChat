37fbbbbb7823b4052a84df460fff705becb4cb29
Hasnain-Mahmood009 <mahammadhasnain8080@gmail.com>
2025-11-26 17:10:16 +0500

    Adding notification feature

--- sw.js
+++ sw.js
@@ -1,19 +1,22 @@
-const CACHE_NAME = "chat-app-cache-v1";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/app.js",
-  "/manifest.json"
-];
+importScripts("https://www.gstatic.com/firebasejs/12.6.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/12.6.0/firebase-messaging-compat.js");
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+firebase.initializeApp({
+    apiKey: "AIzaSyA5daFUKCFePdkZ1Zn1YmhAk39bHu0A6UM",
+    authDomain: "newchatapp-e7614.firebaseapp.com",
+    projectId: "newchatapp-e7614",
+    storageBucket: "newchatapp-e7614.firebasestorage.app",
+    messagingSenderId: "122269204382",
+    appId: "1:122269204382:web:feac24c3b6f2f2ca9b608f",
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+    console.log("Background Message:", payload);
+    self.registration.showNotification(payload.notification.title, {
+        body: payload.notification.body,
+        icon: "final_image.png"
+    });
 });
+
